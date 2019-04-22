fc9a7c3347a34152ab017c427f5c88f2353987e9
yl-1993 <yl93528@gmail.com>
2019-04-22 18:36:11 +0800

    keep script consistent

--- scripts/generate_proposals.sh
+++ scripts/generate_proposals.sh
@@ -1,13 +1,15 @@
 prefix=./data
-oprefix=./data/cluster_proposals
-name='part1_test'
+name=part1_test
+oprefix=$prefix/cluster_proposals
+
 dim=256
 knn=80
-method='faiss'
+method=faiss
 th=0.7
 step=0.05
 minsz=3
 maxsz=300
+metric=pairwise
 
 # generate proposals
 PYTHONPATH=. python proposals/generate_proposals.py \
@@ -25,6 +27,6 @@ PYTHONPATH=. python proposals/generate_proposals.py \
 
 # evaluate
 PYTHONPATH=. python evaluation/evaluate.py \
-    --method 'pairwise' \
+    --metric $metric \
     --gt_labels $prefix/labels/$name.meta \
     --pred_labels $oprefix/$name/$method\_k_$knn\_th_$th\_step_$step\_minsz_$minsz\_maxsz_$maxsz\_iter0/pred_labels.txt
