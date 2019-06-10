35a5fb6639a16d935b3c27251ce6c01a7e629fc3
yl-1993 <yl93528@gmail.com>
2019-06-10 15:56:02 +0800

    update config & script

--- scripts/test_cluster_det.sh
+++ scripts/test_cluster_det.sh
@@ -1,6 +1,6 @@
-PYTHONPATH=. python dsgcn/main.py \
+CUDA_VISIBLE_DEVICES=0 PYTHONPATH=. python dsgcn/main.py \
     --stage det \
     --phase test \
-    --config dsgcn/configs/cfg_0.7_0.75.yaml \
+    --config dsgcn/configs/cfg_test_0.7_0.75.yaml \
     --load_from data/pretrained_models/pretrained_gcn_d.pth.tar \
     --save_output
