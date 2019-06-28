f330032d361d4037174e84d6d3b556021de2f11a
yl-1993 <yl93528@gmail.com>
2019-06-29 00:37:18 +0800

    support diff gcns

--- dsgcn/models/dsgcn.py
+++ dsgcn/models/dsgcn.py
@@ -9,6 +9,12 @@ import torch.nn.functional as F
 
 __all__ = ['dsgcn']
 
+'''
+Original implementation can be referred to:
+    - GCN: https://github.com/tkipf/pygcn
+    - SGC: https://github.com/Tiiiger/SGC
+'''
+
 
 class GraphConv(nn.Module):
 
@@ -64,31 +70,63 @@ class BasicBlock(nn.Module):
         return x
 
 
-class GCN(nn.Module):
+class GNN(nn.Module):
     """ input is (bs, N, D), for featureless, D=1
-        output is (bs, 2)
+        dev output is (bs, num_classes)
+        seg output is (bs, N, num_classes)
     """
 
-    def __init__(self, block, planes, feature_dim, featureless,
+    def __init__(self, planes, feature_dim, featureless,
             num_classes=1, dropout=0.0, reduce_method='max', stage='dev'):
+        assert feature_dim > 0
+        assert dropout >= 0 and dropout < 1
         if featureless:
             self.inplanes = 1
         else:
             self.inplanes = feature_dim
         self.num_classes = num_classes
         self.reduce_method = reduce_method
-        super(GCN, self).__init__()
-
-        assert feature_dim > 0
-        assert dropout >= 0 and dropout < 1
-        self.layers = self._make_layer(block, planes, dropout)
-        self.classifier = nn.Linear(self.inplanes, num_classes)
+        super(GNN, self).__init__()
         if stage == 'dev':
             self.loss = torch.nn.MSELoss()
         elif stage == 'seg':
             self.loss = torch.nn.NLLLoss()
         else:
             raise KeyError('Unknown stage: {}'.format(stage))
+
+    def pool(self, x):
+        # use global op to reduce N
+        # make sure isomorphic graphs output the same representation
+        if self.reduce_method == 'sum':
+            return torch.sum(x, dim=1)
+        elif self.reduce_method == 'mean':
+            return torch.mean(x, dim=1)
+        elif self.reduce_method == 'max':
+            return torch.max(x, dim=1)[0]
+        elif self.reduce_method == 'no_pool':
+            return x # wo global pooling
+        else:
+            raise KeyError('Unkown reduce method', self.reduce_method)
+
+    def forward(self, data, return_loss=False):
+        x = self.extract(data[0], data[1])
+        if return_loss:
+            loss = self.loss(x.view(-1), data[2])
+            return x, loss
+        else:
+            return x
+
+
+class GCN(GNN):
+
+    def __init__(self, planes, feature_dim, featureless,
+            num_classes=1, dropout=0.0, reduce_method='max', stage='dev'):
+        super().__init__(
+                planes, feature_dim, featureless,
+                num_classes, dropout, reduce_method, stage)
+
+        self.layers = self._make_layer(BasicBlock, planes, dropout)
+        self.classifier = nn.Linear(self.inplanes, num_classes)
 
     def _make_layer(self, block, planes, dropout=0.0):
         layers = nn.ModuleList([])
@@ -105,18 +143,7 @@ class GCN(nn.Module):
         assert (D > 0).all(), "D should larger than 0, otherwise gradient will be NaN."
         for layer in self.layers:
             x = layer(x, adj, D)
-        # use global op to reduce N
-        # make sure isomorphic graphs output the same representation
-        if self.reduce_method == 'sum':
-            x = torch.sum(x, dim=1)
-        elif self.reduce_method == 'mean':
-            x = torch.mean(x, dim=1)
-        elif self.reduce_method == 'max':
-            x = torch.max(x, dim=1)[0]
-        elif self.reduce_method == 'no_pool':
-            pass # wo global pooling
-        else:
-            raise KeyError('Unkown reduce method', self.reduce_method)
+        x = self.pool(x)
         x = x.view(-1, self.inplanes)
         x = self.classifier(x)
         if self.reduce_method == 'no_pool':
@@ -128,22 +155,50 @@ class GCN(nn.Module):
                 x = x.view(bs, -1)
         return x
 
-    def forward(self, data, return_loss=False):
-        x = self.extract(data[0], data[1])
-        if return_loss:
-            loss = self.loss(x.view(-1), data[2])
-            return x, loss
-        else:
-            return x
+
+class SGC(GNN):
+
+    def __init__(self, planes, feature_dim, featureless,
+            num_classes=1, dropout=0.0, reduce_method='max', stage='dev'):
+        super().__init__(
+                planes, feature_dim, featureless,
+                num_classes, dropout, reduce_method, stage)
+
+        assert stage == 'dev'
+        self.degree = len(planes)
+        self.classifier = nn.Linear(self.inplanes, num_classes)
+
+    def extract(self, x, adj):
+        adj.detach_()
+        D = adj.sum(dim=2, keepdim=True)
+        D.detach_()
+        assert (D > 0).all(), "D should larger than 0, otherwise gradient will be NaN."
+        for _ in range(self.degree):
+            if x.dim() == 3:
+                x = torch.bmm(adj, x) / D
+            elif x.dim() == 2:
+                x = torch.spmm(adj, x) / D
+        x = self.pool(x)
+        x = self.classifier(x)
+        return x
+
+
+def _build_model(gcn_type):
+    __gcn_type__ = {
+        'gcn': GCN,
+        'sgc': SGC,
+    }
+    if gcn_type not in __gcn_type__:
+        raise KeyError("Unknown gcn_type:", gcn_type)
+    return __gcn_type__[gcn_type]
 
 
 def dsgcn(feature_dim, hidden_dims=[], featureless=True, \
-        reduce_method='max', dropout=0.5, num_classes=1):
-    model = GCN(BasicBlock,
-                planes=hidden_dims,
-                feature_dim=feature_dim,
-                featureless=featureless,
-                reduce_method=reduce_method,
-                dropout=dropout,
-                num_classes=num_classes)
-    return model
+        gcn_type='gcn', reduce_method='max', dropout=0.5, num_classes=1):
+    model = _build_model(gcn_type)
+    return model(planes=hidden_dims,
+                 feature_dim=feature_dim,
+                 featureless=featureless,
+                 reduce_method=reduce_method,
+                 dropout=dropout,
+                 num_classes=num_classes)
