b4d64eb4ea41aa99239d17b9437d5de6d40b8c89
yl-1993 <yl93528@gmail.com>
2020-01-09 19:39:50 +0800

    output colored results

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -5,7 +5,7 @@ import os
 import argparse
 import numpy as np
 
-from utils import Timer
+from utils import Timer, TextColors
 from evaluation import bcubed, pairwise
 
 
@@ -20,6 +20,28 @@ def _read_meta(fn):
     return np.array(labels), lb_set
 
 
+def evaluate(gt_labels, pred_labels, metric='pairwise'):
+    print('[gt_labels] {}'.format(gt_labels))
+    print('[pred_labels] {}'.format(pred_labels))
+    gt_labels, gt_lb_set = _read_meta(gt_labels)
+    pred_labels, pred_lb_set = _read_meta(pred_labels)
+
+    print('#inst: gt({}) vs pred({})'.format(len(gt_labels), len(pred_labels)))
+    print('#cls: gt({}) vs pred({})'.format(len(gt_lb_set), len(pred_lb_set)))
+
+    if metric == 'bcubed':
+        func = bcubed
+    elif metric == 'pairwise':
+        func = pairwise
+    else:
+        raise KeyError('Unsupported evaluation metric', args.metric)
+
+    with Timer('evaluate with {}{}{}'.format(TextColors.FATAL, metric, TextColors.ENDC)):
+        ave_pre, ave_rec, fscore = func(gt_labels, pred_labels)
+    print('{}ave_pre: {:.4f}, ave_rec: {:.4f}, fscore: {:.4f}{}'.\
+            format(TextColors.OKGREEN, ave_pre, ave_rec, fscore, TextColors.ENDC))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Evaluate Cluster')
     parser.add_argument('--gt_labels', type=str, required=True)
@@ -29,20 +51,4 @@ if __name__ == '__main__':
                         choices=['pairwise', 'bcubed'])
     args = parser.parse_args()
 
-    gt_labels, gt_lb_set = _read_meta(args.gt_labels)
-    pred_labels, pred_lb_set = _read_meta(args.pred_labels)
-
-    print('#inst: gt({}) vs pred({})'.format(len(gt_labels), len(pred_labels)))
-    print('#cls: gt({}) vs pred({})'.format(len(gt_lb_set), len(pred_lb_set)))
-
-    if args.metric == 'bcubed':
-        func = bcubed
-    elif args.metric == 'pairwise':
-        func = pairwise
-    else:
-        raise KeyError('Unsupported evaluation metric', args.metric)
-
-    with Timer('evaluate with {}'.format(args.metric)):
-        ave_pre, ave_rec, fscore = func(gt_labels, pred_labels)
-    print('ave_pre: {:.4f}, ave_rec: {:.4f}, fscore: {:.4f}'.\
-            format(ave_pre, ave_rec, fscore))
+    evaluate(args.gt_labels, args.pred_labels, args.metric)
