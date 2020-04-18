fd06bc890cefe8deb07c7919dd976707fbaece8c
yl-1993 <yl93528@gmail.com>
2020-04-18 16:19:43 +0800

    add gcn_e test on ms1m

---
+++ scripts/vegcn/test_gcn_e.sh
@@ -0,0 +1,13 @@
+config=vegcn/configs/cfg_test_gcne_ms1m.py
+load_from=data/pretrained_models/pretrained_gcn_e.pth
+
+export CUDA_VISIBLE_DEVICES=0
+export PYTHONPATH=.
+
+python vegcn/main.py \
+    --config $config \
+    --phase 'test' \
+    --load_from $load_from \
+    --save_output \
+    --eval_interim \
+    --force
